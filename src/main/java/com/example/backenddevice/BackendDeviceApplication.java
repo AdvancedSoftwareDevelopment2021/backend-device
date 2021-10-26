daa436c66173873b45692d452e04f86939556aab
Dyanjun <dyanjun15201981339@163.com>
2021-10-26 23:32:28 +0800

    初始化设备端代码

---
+++ src/main/java/com/example/backenddevice/BackendDeviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.backenddevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendDeviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backenddevice/BackendDeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backenddevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDeviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
